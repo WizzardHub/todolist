358db1af7a5fffa3ac040d2a79f0c13b0435b193
WizzardHub <contact@wizzardr.dev>
2023-04-20 10:18:24 +0200

    Step #2

--- App.js
+++ App.js
@@ -1,23 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.bold}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>What shall i add today ?</Text>
+      <View style={[styles.row, styles.marginTop]}>
+        <TextInput placeholder="Ajouter" style={styles.input} />
+        <Button title="Add" color='#9bebc3' textColor='#1cb066' />
+      </View>
       <StatusBar style="auto" />
     </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
     justifyContent: 'center',
   },
-  bold: {
+
+  title: {
+    padding: 5,
+    fontSize: 24,
+    borderRadius: 5,
+    color: '#1cb066',
     fontWeight: 'bold',
-  }
+    backgroundColor: '#9bebc3',
+  },
+
+  row: {
+    flexDirection: 'row',
+  },
+
+  marginTop: {
+    marginTop: 16
+  },
+
+  input: {
+    height: 40,
+    width: 200,
+    padding: 10,
+    borderWidth: 3,
+    borderRadius: 5,
+    marginRight: 10,
+    borderColor: '#aefcd5',
+  },
 });
+
+const sampleGoals = [
+  "Faire les courses",
+  "Aller à la salle de sport 3 fois par semaine",
+  "Monter à plus de 5000m d'altitude",
+  "Acheter mon premier appartement",
+  "Perdre 5kgs",
+  "Gagner en productivité",
+  "Apprendre un nouveau langage",
+  "Faire une mission en freelance",
+  "Organiser un meetup autour de la tech",
+  "Faire un triathlon",
+]
