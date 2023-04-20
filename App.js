29246800e94c9d3bab5a3c09dfd01734ed09e709
WizzardHub <contact@wizzardr.dev>
2023-04-20 13:46:01 +0200

    Fix # 1 / Step # 2 (Forgot to show the list of todos)

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>What shall i add today ?</Text>
+      <Text style={styles.title}>#TODO</Text>
       <View style={[styles.row, styles.marginTop]}>
-        <TextInput placeholder="Ajouter" style={styles.input} />
+        <TextInput placeholder="What shall i add today ?" style={styles.input} />
         <Button title="Add" color='#9bebc3' textColor='#1cb066' />
       </View>
+      <FlatList
+        style={styles.list}
+        data={sampleGoals}
+        renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+      />
       <StatusBar style="auto" />
     </View>
     );
@@ -48,6 +53,20 @@ const styles = StyleSheet.create({
     marginRight: 10,
     borderColor: '#aefcd5',
   },
+
+  list: {
+    backgroundColor: '#70dba5',
+    maxHeight: 150,
+    marginTop: 20,
+    borderRadius: 5,
+    paddingHorizontal: 5
+  },
+
+  item: {
+    padding: 5,
+    color: '#c8f7df',
+    fontWeight: '500',
+  }
 });
 
 const sampleGoals = [
