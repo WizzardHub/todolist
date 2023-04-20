c6bfd5503061823e24cb3f2acb16f7fad458f7b3
WizzardHub <contact@wizzardr.dev>
2023-04-20 15:36:28 +0200

    Step #3

--- App.js
+++ App.js
@@ -1,18 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import {Button, FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, FlatList, Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodos] = useState(sampleGoals);
+
+  const onPressAdd = () => {
+    if (inputValue.length >= 5) {
+      setTodos([...todos, inputValue]) // Adding the value of the input to the array
+      setInputValue('') // Resetting the input value
+    }
+  }
+
+  const onPressRemove = (index) => {
+    setTodos(
+      todos.filter((todo, i) => i !== index))
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>#TODO</Text>
       <View style={[styles.row, styles.marginTop]}>
-        <TextInput placeholder="What shall i add today ?" style={styles.input} />
-        <Button title="Add" color='#9bebc3' textColor='#1cb066' />
+        <TextInput
+          style={styles.input}
+          value={inputValue} // Keep track of the current value
+          placeholder="What shall i add today ?"
+          onChangeText={text => setInputValue(text)} // Update the value
+        />
+
+        <Button title="Add" color="#9bebc3" textColor="#1cb066" onPress={onPressAdd} />
       </View>
       <FlatList
         style={styles.list}
-        data={sampleGoals}
-        renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+        data={todos} // The value of the useState
+        renderItem={({item, index}) =>
+          <View style={[styles.row, styles.spaceBetween]}>
+            <Text style={styles.item}>{item}</Text>
+            <Pressable style={styles.miniButton} onPress={() => onPressRemove(index)}>
+              <Text style={styles.miniButtonText}>Retirer</Text>
+            </Pressable>
+          </View>
+        }
       />
       <StatusBar style="auto" />
     </View>
@@ -40,13 +70,18 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
 
+  spaceBetween: {
+    justifyContent: 'space-between',
+    alignItems: 'flex-end'
+  },
+
   marginTop: {
     marginTop: 16
   },
 
   input: {
     height: 40,
-    width: 200,
+    width: 290,
     padding: 10,
     borderWidth: 3,
     borderRadius: 5,
@@ -56,7 +91,7 @@ const styles = StyleSheet.create({
 
   list: {
     backgroundColor: '#70dba5',
-    maxHeight: 150,
+    maxHeight: 200,
     marginTop: 20,
     borderRadius: 5,
     paddingHorizontal: 5
@@ -66,6 +101,18 @@ const styles = StyleSheet.create({
     padding: 5,
     color: '#c8f7df',
     fontWeight: '500',
+  },
+
+  miniButton: {
+    margin: 5,
+    backgroundColor: 'rgba(255, 255, 255, .5)',
+    borderRadius: 5,
+    paddingHorizontal: 5,
+  },
+
+  miniButtonText: {
+    color: 'rgba(0, 0, 0, .4)',
+    fontWeight: 'bold'
   }
 });
 
